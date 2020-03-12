bcaa9bf0ffcc75fae95e57282c801cc494bf593f
Kirill Kiselev <skulpin@mail.ru>
2020-03-12 13:21:12 +0700

    menu adaptive changes

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,14 +1,33 @@
+
 const menu = () => {
+  const getAdaptiveTransform = () => {
+    const transformVisibleX = 'translate3d(100%,0,0)',
+      transformVisibleY = 'translate3d(0,-100%,0)';
+    
+    const windowWidth = document.documentElement.clientWidth,
+      mobileWidth = 576;
+  
+    if (windowWidth < mobileWidth) {
+      return transformVisibleY;
+    } else {
+      return transformVisibleX;
+    }
+  }
+
   const button = document.querySelector('.menu__icon'),
     popup = document.querySelector('.popup-menu'),
     menu = document.querySelector('.popup-dialog-menu'),
     close = document.querySelector('.close-menu');
+  
+  let transform = 'translate3d(0,0,0)';
+
+  menu.style.visibility = 'hidden';
 
   const openMenu = () => {
     const visibility = 'visible';
   
     popup.style.paddingRight = 0;
-    menu.style.transform = 'translate3d(0,0,0)';
+    menu.style.transform = transform;
     menu.style.visibility = visibility;
     popup.style.visibility = visibility;
   }
@@ -17,7 +36,11 @@ const menu = () => {
     const visibility = 'hidden';
   
     popup.style.paddingRight = '10px';
-    menu.style.transform = 'translate3d(100%,0,0)';
+    menu.style.transform = getAdaptiveTransform();
+    
+    popup.removeAttribute('style');
+    menu.removeAttribute('style');
+
     menu.style.visibility = visibility;
     popup.style.visibility = visibility;
   }
