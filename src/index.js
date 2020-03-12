e1d5a8cb0039c293d5f1b43eafc4f47579af8975
Kirill Kiselev <skulpin@mail.ru>
2020-03-12 14:33:54 +0700

    changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import phones from './modules/phones';
 import menu from './modules/menu';
+import popupTypes from './modules/popupTypes';
 
 phones();
 menu();
+popupTypes();
---
+++ src/modules/popupTypes.js
@@ -0,0 +1,11 @@
+import menu from "./menu";
+
+const popupTypes = () => {
+  menu();
+
+  openMenu();
+  // Нажимаем на Полный список услуг и цен, появляется попап
+  // Скрываем меню с помощью closeMenu() который находится в модуле menu()
+};
+
+export default popupTypes;
