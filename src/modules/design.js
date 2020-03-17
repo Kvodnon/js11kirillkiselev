c631be2a83bce6b2c674aa9f82634e159135f50f
Kirill Kiselev <skulpin@mail.ru>
2020-03-17 17:36:57 +0700

    changes

--- src/modules/design.js
+++ src/modules/design.js
@@ -2,12 +2,37 @@ import { sliderCounter, SliderCarousel } from "./carousel";
 
 const sliders = document.querySelector('.designs-slider');
 
-const design = () => {
-  const child = sliders.children[0];
+let designSlider,
+  designSliders = [];
 
-  const designSlider = new SliderCarousel({
-    main: `.designs-slider`,
-    wrap: `.${child.className}`,
+const design = () => {
+  const children = sliders.children;
+  
+  for (const child of children) {
+    const slider = new SliderCarousel({
+      main: `.repair-types-slider`,
+      wrap: `.${child.className}`,
+      
+      prev: '#repair-types-arrow_left',
+      next: '#repair-types-arrow_right',
+      
+      numberSlider: sliderCounter.count,
+      slidesToShow: 1,
+    });
+    
+    slider.init();
+    // slider.removeControl();
+    
+    designSliders.push(slider);
+  
+    // slider.wrap.addEventListener('sliderChange', () => {
+    //   current.innerText = slider.options.position + 1;
+    // });
+  }
+
+  designSlider = new SliderCarousel({
+    main: `.designs-slider-wrap`,
+    wrap: `.designs-slider`,
   
     tabs: '#designs-list',
     tabClass: 'designs-nav__item',
